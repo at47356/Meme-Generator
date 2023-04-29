dc92bf2c60afb689e96cfc9a1682c439a4f93803
at47356 <101987474+at47356@users.noreply.github.com>
2023-04-29 17:36:51 +0100

    Add Image

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,9 +1,25 @@
-export default function input() {
+import memesData from "./memesData"
+import {useState} from 'react'
+
+export default function Input() {
+    const [memeImage, setMemeImage] = useState("")
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+     
+
     return (
-        <div className="interactive-area">
-            <input className="input-box"type="text" placeholder="Top Line"/>
-            <input className="input-box"type="text" placeholder="Bottom Line"/>
-            <button className="generation-button">Get a new meme image  🖼</button>
-        </div>
+        <main>
+            <div className="interactive-area">
+                <input className="input-box"type="text" placeholder="Top Line"/>
+                <input className="input-box"type="text" placeholder="Bottom Line"/>
+                <button className="generation-button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+            </div>
+
+            <img src={memeImage} className="memeImage"/>
+        </main>
     )
 }
